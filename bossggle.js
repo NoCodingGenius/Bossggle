585a3e6ba94dc2c536cbe2522d9bbaecc6a2a3c2
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-05-09 15:50:02 -0700

    Updated the name of the document to bossggle.js and changed the color to orange

---
+++ bossggle.js
@@ -0,0 +1,32 @@
+//Change button color to orange when clicked
+var question = document.querySelectorAll('.btn1');
+
+function myColor(){
+  this.style.backgroundColor = 'orange';
+}
+
+for ( var i=0; i<question.length; i++){
+  question[i].addEventListener('click', myColor)
+}
+
+//A function to get the ID and puts the possible letters in the ID
+function getWord () {
+  var letterContainer = document.querySelector('.btn1');
+  var letter = letterContainer.innerText;
+  document.getElementById("word").textContent = letter;
+}
+
+function clearWord() {
+  document.getElementById("word").textContent = "Word"
+}
+
+function randomLetter() {
+  alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  var emptyString = emptyString + getRandomLetter;
+  document.getElementsByClassName('btn1').textContent = emptyString;
+}
+
+function randomVowel() {
+
+}
