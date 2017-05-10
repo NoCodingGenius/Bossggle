06caaffcefb12f54615866e5615dd9bb743393f0
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-05-09 17:34:46 -0700

    changed files to appropriate branch

---
+++ bossggle.js
@@ -0,0 +1,46 @@
+//A function to get the ID and puts the possible letters in the ID
+function getWord ( ) {
+  var letterContainer = document.querySelectorAll('.btn')[i].innerText;
+  var letter = letterContainer.innerText
+  document.getElementById("word").innerHTML = letter;
+}
+
+function getLetter(){
+  var letterContainer = document.querySelectorAll('.btn')[i].innerText;
+}
+
+for ( var i = 0; i < question.length; i++){
+  question[i].addEventListener('click', myColor)
+}
+//when i click any particular button
+//grab its innerHtml
+/*for ( var i=0; i<question.length; i++){
+  question[i].addEventListener('click', myColor)
+}*/
+
+//Change button color to orange when clicked
+var question = document.querySelectorAll('.btn');
+
+function myColor(){
+  this.style.backgroundColor = 'orange';
+}
+
+for ( var i=0; i<question.length; i++){
+  question[i].addEventListener('click', myColor)
+}
+
+//A function to clear the board
+function clearWord() {
+  document.getElementById("word").textContent = "Word"
+}
+//A Function to generate random letters when the board is reset
+function randomLetter() {
+  alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  var emptyString = emptyString + getRandomLetter;
+  document.getElementsByClassName('btn').textContent = emptyString;
+}
+//A function to generate a random vowel
+function randomVowel() {
+
+}
